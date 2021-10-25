92a43c515e723f34261c18f6652da1a0e79d5903
Ady Abraham <adyabr@google.com>
2021-10-25 09:31:57 +0800

    lavender: SurfaceFlinger: Enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing.
    
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8
    [clarencelol: As we disable debug.sf.disable_backpressure, we need debug.sf.enable_gl_backpressure to improve our hwc]
    
    Change-Id: Ic2dffa104b86bf9442a846380dc9054fd9c80184
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -208,6 +208,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_gl_backpressure=1 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096
